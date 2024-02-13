ae6ef02d9ae4dfe137a43929d7b19201f64dea12
Brooklyn Mcswiney <brooklynmcswiney@gmail.com>
2024-02-13 17:13:59 +0000

    added path for get_story

--- cwk1/cwk1api/api/views.py
+++ cwk1/cwk1api/api/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import HttpResponse
+from django.http import JsonResponse
+import json
 
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, world. You're at the api index.")
+
+def get_story(request):
+    return JsonResponse({"status": 200})
