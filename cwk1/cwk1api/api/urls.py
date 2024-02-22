1de58e17cbc2767b9416830097c25a1de5b05cf8
Brooklyn Mcswiney <brooklynmcswiney@gmail.com>
2024-02-22 15:51:27 +0000

    started updating get api/stories to use data encode format

--- cwk1/cwk1api/api/urls.py
+++ cwk1/cwk1api/api/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path("login", views.login, name="login"),
     path("logout", views.logout, name="logout"),
-    path("stories", views.stories, name="get_story"),
+    path("stories", views.stories, name="stories"),
     path("stories/<int:id>", views.delete_story, name="delete_story"),
 ]
--- cwk1/cwk1api/api/views.py
+++ cwk1/cwk1api/api/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import HttpResponse
 from django.http import JsonResponse
 from django.conf import settings
 from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
 from django.views.decorators.csrf import csrf_exempt
 from api.models import Story
+from datetime import date
 import json
 
 # Create your views here.
@@ -14,14 +16,16 @@ def index(request):
 def login(request):
 
     # Get request body
-    request_dict = request.body.decode('utf-8')
-    body = json.loads(request_dict)
+    username = request.POST["username"]
+    password = request.POST["password"]
+
 
     # Authenticate user
-    user = authenticate(request, username=body["username"], password=body["password"])
+    user = authenticate(request, username=username, password=password)
 
     if user is not None:
         request.session["login_status"] = "logged_in"
+        request.session["username"] = password
 
         return JsonResponse({"status": 200, "message": "Welcome!"})
 
@@ -35,6 +39,8 @@ def logout(request):
 
         if session == "logged_in":
             request.session["login_status"] = "logged_out"
+            request.session["username"] = ""
+
             return JsonResponse({"status": 200, "message": "User logged out"})
     except KeyError:
         return JsonResponse({"status": 401, "message": "User not logged in"})
@@ -43,54 +49,89 @@ def logout(request):
 
 @csrf_exempt
 def stories(request):
-    if request.method == "GET":
-        return get_story(request)
+    # if request.method == "GET":
+    #     return get_story(request)
     
-    if request.method == "POST":
-        return post_story(request)
+    return get_story(request)
+
+    # if request.method == "POST":
+    #     return post_story(request)
 
 def post_story(request):
 
     try:
         session = request.session["login_status"]
 
-        if session == "logged_in":
-            return HttpResponse("logged in")
+        if session != "logged_in":
+            return JsonResponse({"status": 503, "message": "Service unavailable, user not logged in"})
     except KeyError:
-        return JsonResponse({"status": 401, "message": "User not logged in"})
+        return JsonResponse({"status": 503, "message": "Service unavailable, user not logged in"})
     
-    return HttpResponse("Post story not implemented")
+    request_dict = request.body.decode('utf-8')
+    body = json.loads(request_dict)
+
+    headline = body["headline"]
+    category = body["category"]
+    region = body["region"]
+    details = body["details"]
+    date_today = date.today()
+    author = User.objects.get(username=request.session["username"])
+
+    if headline == "" or category == "" or region == "" or details == "":
+        return JsonResponse({"status": 503, "message": "Service unavailable, empty fields detected"})
+
+    if category != "pol" and category != "art" and category != "tech" and category != "trivia":
+        return JsonResponse({"status": 503, "message": "Service unavailable, invalid category"})
+    
+    if region != "uk" and region != "eu" and region != "w":
+        return JsonResponse({"status": 503, "message": "Service unavailable, invalid region"})
+    
+
+
+    story = Story(headline=headline, category=category, region=region, details=details, date=date_today, author=author)
+
+    story.save()
+
+    return JsonResponse({"status": 201, "message": "CREATED"})
 
 def get_story(request):
 
-    # Get request body
-    request_dict = request.body.decode('utf-8')
-    body = json.loads(request_dict)
+    # Get request data
+    category = request.POST["story_cat"]
+    region = request.POST["story_region"]
+    date = request.POST["story_date"]
 
     # Filter stories by request specified params
     response = Story.objects
 
-    if body["story_cat"] == "*":
+    if category == "*":
         response = response.all()
     else:
-        response = response.filter(category=body["story_cat"])
+        response = response.filter(category=category)
     
-    if body["story_region"] == "*":
+    if region == "*":
         response = response.all()
     else:  
-        response = response.filter(region=body["story_region"])
+        response = response.filter(region=region)
     
-    if body["story_date"] == "*":
+    if date == "*":
         response = response.all()
     else:
-        response = response.filter(date=body["story_date"])
+        response = response.filter(date=date)
 
-    return JsonResponse({"status": 200, "stories": list(response.values())})
+    return JsonResponse({"status": 200, "stories": list(response.values()), "story_cat": category, "story_region": region, "story_date": date})
+    # return HttpResponse(request.encoding)
 
 def delete_story(request, id):
 
     if request.method != "DELETE":
-        return JsonResponse({"status": 400, "message": "Invalid request method"})
+        return JsonResponse({"status": 503, "message": "Service unavailable, Invalid method"})
 
-    return HttpResponse("Delete story not implemented")
+    item = Story.objects.get(id=id)
+
+    if item is not None:
+        item.delete()
+        return JsonResponse({"status": 200, "message": "OK"})
+
+    return JsonResponse({"status": 503, "message": "Service unavailable, Invalid method"})
 
